2542a6a12ed9c034f532a6b68053f6d0dcbcb1ab
Matthias Zimmermann <admin@matthiasz.com>
2021-12-07 08:53:06 +0100

    Solution Day 1.2

--- Solution/Day1.py
+++ Solution/Day1.py
@@ -1,13 +1,45 @@
 from Input.Day1 import inputMeasurements
 
 def SolveDay1():
-	print (CalculateIncreses(inputMeasurements))
+	input0 = inputMeasurements.copy()
+	input1 = inputMeasurements.copy()
+	input2 = inputMeasurements.copy()
 
-def CalculateIncreses(input):
-	previousInput = input.pop()
+	print ("Day 1.1: %d" %SolutionDay1_1(input0))
+	print ("Day 1.2: %d" %SolutionDay1_2forloop(input1))
+	print ("Day 1.2: %d" %SolutionDay1_2index(input2))
+
+def SolutionDay1_1(input):
+	previousInput = input.pop(0)
 	n = 0
+
 	for i in input:
 		if i > previousInput:
 			n += 1
 		previousInput = i
+
+	return n
+
+def SolutionDay1_2forloop(input):
+	previousInput2 = input.pop(0)
+	previousInput1 = input.pop(0)
+	previousInput0 = input.pop(0)
+	n = 0
+	
+	for i in input:
+		if i > previousInput2:
+			n += 1
+		previousInput2 = previousInput1
+		previousInput1 = previousInput0
+		previousInput0 = i
+
+	return n
+
+def SolutionDay1_2index(input):
+	n = 0
+	for i in range(0, len(input)-3):
+
+		if input[i+3] > input[i]:
+			n += 1
+
 	return n
