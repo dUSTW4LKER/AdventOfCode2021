c02764a04f08eb96d80999f088dde6576449bdb1
Matthias Zimmermann <admin@matthiasz.com>
2021-12-07 09:47:58 +0100

    Solution Day 2.2

--- Solution/Day2.py
+++ Solution/Day2.py
@@ -3,8 +3,11 @@ from Input.Day2 import day2InputData
 
 def SolveDay2():
 	input0 = day2InputData.copy()
+	input1 = day2InputData.copy()
 
 	print("Day 2.1: " + SolutionDay2_1(input0))
+	print("Day 2.2: " + SolutionDay2_2(input1))
+
 
 def SolutionDay2_1(input):
 	depth = 0
@@ -29,3 +32,28 @@ def SolutionDay2_1(input):
 
 	return (f"{horizontalPosition} x {depth} = {ProblemResult}")
 
+
+def SolutionDay2_2(input):
+	depth = 0
+	horizontalPosition = 0
+	aim = 0
+
+	for i in input:
+		splitInput = i.split()
+		direction = splitInput[0]
+		value = int(splitInput[1])
+
+		match direction:
+			case "forward":
+				horizontalPosition += value
+				depth = depth + aim * value
+			case "down":
+				aim += value
+			case "up":
+				aim -= value
+			case _:
+				print("match error")
+
+	ProblemResult = horizontalPosition * depth
+
+	return (f"{horizontalPosition} x {depth} = {ProblemResult}")
